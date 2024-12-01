c8517ee2747d8fa1ff1e48645f33f08800149f76
guilherme_novaes <guilherme.novaes233@gmail.com>
2024-12-01 18:54:09 -0300

    feature: adding functional tests

--- tests/Fiap.McTech.Cart.FunctionalTests/Fiap.McTech.Cart.FunctionalTests/StepDefinitions/CartStepDefinitions.cs
+++ tests/Fiap.McTech.Cart.FunctionalTests/Fiap.McTech.Cart.FunctionalTests/StepDefinitions/CartStepDefinitions.cs
@@ -1,168 +1,140 @@
-﻿//using Fiap.McTech.Cart.Api.Controllers;
-//using Fiap.McTech.Cart.Api.DbContext;
-//using Fiap.McTech.Cart.Api.Dtos;
-//using Fiap.McTech.Cart.Api.Entities;
-//using Microsoft.AspNetCore.Mvc;
-//using NUnit.Framework;
-//using StackExchange.Redis;
-//using System.Net;
+﻿using Fiap.McTech.Cart.Api.Controllers;
+using Fiap.McTech.Cart.Api.DbContext;
+using Fiap.McTech.Cart.Api.Dtos;
+using Fiap.McTech.Cart.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using StackExchange.Redis;
+using System.Text.Json;
 
-//namespace FunctionalTests.StepDefinitions
-//{
-//    [Binding]
-//    public class CartStepDefinitions
-//    {
-//        private readonly CartController _controller;
-//        private IActionResult _response;
-//        private Guid _clientId = Guid.NewGuid();
-//        private CartItemDto _itemDto;
-//        private IRedisDataContext _redisDataContext;
+namespace FunctionalTests.StepDefinitions
+{
+    [Binding]
+    public class CartStepDefinitions
+    {
+        private readonly Mock<IRedisDataContext> _redisDataContextMock;
+        private readonly CartController _controller;
+        private IActionResult _response;
+        private readonly Guid _clientId;
+        private Mock<IDatabase> _redisDbMock;
 
-//        public CartStepDefinitions()
-//        {
-//            var redisConnectionString = "localhost:6379";
-//            var connection = ConnectionMultiplexer.Connect(redisConnectionString);
-//            _redisDataContext = new RedisDataContext(connection);
+        public CartStepDefinitions()
+        {
+            _redisDbMock = new Mock<IDatabase>();
+            _redisDataContextMock = new Mock<IRedisDataContext>();
+            _redisDataContextMock.Setup(r => r.Database).Returns(_redisDbMock.Object);
+            _controller = new CartController(_redisDataContextMock.Object);
+            _clientId = Guid.NewGuid();
+        }
 
-//            _controller = new CartController(_redisDataContext);
-//        }
+        [Given(@"que eu tenho um ID de cliente único")]
+        public void GivenQueEuTenhoUmIDDeClienteUnico()
+        {
+            // No actions needed here
+        }
 
-//        [Given(@"que eu tenho um ID de cliente único")]
-//        public void GivenQueEuTenhoUmIDDeClienteUnico()
-//        {
-//            _clientId = Guid.NewGuid();
-//        }
+        [When(@"eu crio um novo carrinho para o cliente")]
+        public async Task WhenEuCrioUmNovoCarrinhoParaOCliente()
+        {
+            _redisDbMock
+                .Setup(r => r.StringGetAsync(It.IsAny<RedisKey>(), CommandFlags.None))
+                .ReturnsAsync(RedisValue.Null);
 
-//        [When(@"eu crio um novo carrinho para o cliente")]
-//        public async Task WhenEuCrioUmNovoCarrinhoParaOCliente()
-//        {
-//            _response = await _controller.CreateCart(_clientId);
-//        }
+            _response = await _controller.CreateCart(_clientId);
+        }
 
-//        [Then(@"o status da resposta deve ser (.*) Created")]
-//        public void ThenOStatusDaRespostaDeveSerCreated(int statusCode)
-//        {
-//            var createdAtResult = _response as CreatedAtActionResult;
-//            Assert.AreEqual(statusCode, createdAtResult.StatusCode);
-//        }
+        [Then(@"o status da resposta deve ser (.*) Created")]
+        public void ThenOStatusDaRespostaDeveSerCreated(int statusCode)
+        {
+            var createdResult = _response as CreatedAtActionResult;
+            Assert.IsNotNull(createdResult);
+            Assert.AreEqual(statusCode, createdResult?.StatusCode);
+        }
 
-//        [Then(@"o carrinho deve existir no sistema")]
-//        public async Task ThenOCarrinhoDeveExistirNoSistema()
-//        {
-//            var result = await _controller.GetCart(_clientId) as OkObjectResult;
-//            Assert.IsNotNull(result);
-//            Assert.AreEqual((int) HttpStatusCode.OK, result?.StatusCode);
-//        }
+        [Then(@"o carrinho deve existir no sistema")]
+        public async Task ThenOCarrinhoDeveExistirNoSistema()
+        {
+            _redisDbMock
+                .Verify(r => r.StringSetAsync(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), null, false, When.Always, CommandFlags.None), Times.Once);
 
-//        [Given(@"que um carrinho existe para um cliente")]
-//        public async Task GivenQueUmCarrinhoExisteParaUmCliente()
-//        {
-//            await _controller.CreateCart(_clientId);
-//        }
+            var cartData = JsonSerializer.Serialize(new CartClient(_clientId));
+            var redisValue = new RedisValue(cartData);  
 
-//        [When(@"eu recupero o carrinho para o cliente")]
-//        public async Task WhenEuRecuperoOCarrinhoParaOCliente()
-//        {
-//            _response = await _controller.GetCart(_clientId);
-//        }
+            _redisDbMock
+                .Setup(r => r.StringGetAsync(It.IsAny<RedisKey>(), CommandFlags.None))
+                .ReturnsAsync(redisValue);
 
-//        [Then(@"o status da resposta deve ser (.*) OK")]
-//        public void ThenOStatusDaRespostaDeveSerOK(int statusCode)
-//        {
-//            var result = _response as OkObjectResult;
-//            Assert.AreEqual((int) HttpStatusCode.OK, result?.StatusCode);
-//        }
+            var result = await _controller.GetCart(_clientId);
+            var okResult = result as OkObjectResult;
 
-//        [Then(@"os Dados do carrinho devem corresponder ao formato esperado")]
-//        public void ThenOsDadosDoCarrinhoDevemCorresponderAoFormatoEsperado()
-//        {
-//            var cart = ((OkObjectResult) _response).Value as CartClient;
-//            Assert.IsNotNull(cart);
-//            Assert.AreEqual(_clientId, cart?.ClientId);
-//        }
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult?.StatusCode);
+        }
 
-//        [When(@"eu adiciono um item ao carrinho com detalhes específicos")]
-//        public async Task WhenEuAdicionoUmItemAoCarrinhoComDetalhesEspecificos()
-//        {
-//            _itemDto = new CartItemDto("Produto Exemplo", 2, 10.0M, Guid.NewGuid(), Guid.NewGuid());
-//            _response = await _controller.AddItem(_clientId, _itemDto);
-//        }
+        [Given(@"que um carrinho existe para um cliente")]
+        public void GivenQueUmCarrinhoExisteParaUmCliente()
+        {
+            var cartData = JsonSerializer.Serialize(new CartClient(_clientId));
+            _redisDbMock
+                .Setup(r => r.StringGetAsync(It.IsAny<RedisKey>(), CommandFlags.None))
+                .ReturnsAsync(cartData); 
+        }
 
-//        [Given(@"que um carrinho com itens existe para um cliente")]
-//        public async Task GivenQueUmCarrinhoComItensExisteParaUmCliente()
-//        {
-//            _clientId = Guid.NewGuid();
-//            await _controller.CreateCart(_clientId);
-//            _itemDto = new CartItemDto("Produto Exemplo", 2, 10.0M, Guid.NewGuid(), Guid.NewGuid());
+        [When(@"eu recupero o carrinho para o cliente")]
+        public async Task WhenEuRecuperoOCarrinhoParaOCliente()
+        {
+            _response = await _controller.GetCart(_clientId);
+        }
 
-//            await _controller.AddItem(_clientId, _itemDto);
+        [Then(@"o status da resposta deve ser (.*) OK")]
+        public void ThenOStatusDaRespostaDeveSerOK(int statusCode)
+        {
+            var okResult = _response as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(statusCode, okResult?.StatusCode);
+        }
 
-//            var cartResponse = await _controller.GetCart(_clientId) as OkObjectResult;
-//            var cart = cartResponse?.Value as CartClient;
+        [Then(@"os Dados do carrinho devem corresponder ao formato esperado")]
+        public void ThenOsDadosDoCarrinhoDevemCorresponderAoFormatoEsperado()
+        {
+            var result = _response as OkObjectResult;
+            Assert.IsNotNull(result);
+            var cart = result?.Value as CartClient;
+            Assert.IsNotNull(cart);
+            Assert.AreEqual(_clientId, cart?.ClientId);
+        }
 
-//            Assert.IsNotNull(cart);
-//            Assert.IsTrue(cart?.Items.Any(i => i.ProductId == _itemDto.ProductId));
-//        }
+        [When(@"eu adiciono um item ao carrinho com detalhes específicos")]
+        public async Task WhenEuAdicionoUmItemAoCarrinhoComDetalhesEspecificos()
+        {
+            var itemDto = new CartItemDto(name: "Item Exemplo", quantity: 2, value: 15.99m, productId: Guid.NewGuid(), _clientId);
 
-//        [Then(@"o item deve aparecer no carrinho com os detalhes corretos")]
-//        public async Task ThenOItemDeveAparecerNoCarrinhoComOsDetalhesCorretos()
-//        {
-//            var cartResponse = await _controller.GetCart(_clientId) as OkObjectResult;
-//            var cart = cartResponse?.Value as CartClient;
-//            var item = cart?.Items.Find(i => i.ProductId == _itemDto.ProductId);
+            var cartData = JsonSerializer.Serialize(new CartClient(_clientId));
+            var redisValue = new RedisValue(cartData);
 
-//            Assert.IsNotNull(item);
-//            Assert.AreEqual(_itemDto.Name, item?.Name);
-//            Assert.AreEqual(_itemDto.Quantity, item?.Quantity);
-//            Assert.AreEqual(_itemDto.Value, item?.Value);
-//        }
+            _redisDbMock
+                .Setup(r => r.StringGetAsync(It.IsAny<RedisKey>(), CommandFlags.None))
+                .ReturnsAsync(redisValue);
 
-//        [When(@"eu atualizo a quantidade de um item no carrinho")]
-//        public async Task WhenEuAtualizoAQuantidadeDeUmItemNoCarrinho()
-//        {
-//            _response = await _controller.UpdateItemQuantity(_clientId, _itemDto.ProductId, 5);
-//        }
+            _response = await _controller.AddItem(_clientId, itemDto);
+        }
 
-//        [Then(@"a quantidade do item deve ser atualizada no carrinho")]
-//        public async Task ThenAQuantidadeDoItemDeveSerAtualizadaNoCarrinho()
-//        {
-//            var cartResponse = await _controller.GetCart(_clientId) as OkObjectResult;
-//            var cart = cartResponse?.Value as CartClient;
-//            var item = cart?.Items.Find(i => i.ProductId == _itemDto.ProductId);
+        [Then(@"o item deve aparecer no carrinho com os detalhes corretos")]
+        public void ThenOItemDeveAparecerNoCarrinhoComOsDetalhesCorretos()
+        {
+            var okResult = _response as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(200, okResult?.StatusCode);
 
-//            Assert.IsNotNull(item);
-//            Assert.AreEqual(5, item?.Quantity);
-//        }
+            var cart = okResult?.Value as CartClient;
+            Assert.IsNotNull(cart);
 
-//        [When(@"eu removo um item do carrinho")]
-//        public async Task WhenEuRemovoUmItemDoCarrinho()
-//        {
-//            _response = await _controller.RemoveItem(_clientId, _itemDto.ProductId);
-//        }
-
-//        [Then(@"o item não deve mais existir no carrinho")]
-//        public async Task ThenOItemNaoDeveMaisExistirNoCarrinho()
-//        {
-//            var cartResponse = await _controller.GetCart(_clientId) as OkObjectResult;
-//            var cart = cartResponse?.Value as CartClient;
-//            var item = cart?.Items.Find(i => i.ProductId == _itemDto.ProductId);
-
-//            Assert.IsNull(item);
-//        }
-
-
-//        [When(@"eu obtenho o valor total do carrinho")]
-//        public async Task WhenEuObtenhoOValorTotalDoCarrinho()
-//        {
-//            _response = await _controller.GetTotalValue(_clientId);
-//        }
-
-//        [Then(@"o valor total deve estar correto")]
-//        public void ThenOValorTotalDeveEstarCorreto()
-//        {
-//            var totalResponse = _response as OkObjectResult;
-//            Assert.IsNotNull(totalResponse);
-//            Assert.AreEqual(20.0M, totalResponse?.Value);
-//        }
-//    }
-//}
+            var item = cart?.Items.SingleOrDefault(i => i.ProductId == cart?.Items.First().ProductId);
+            Assert.IsNotNull(item);
+            Assert.AreEqual("Item Exemplo", item?.Name);
+            Assert.AreEqual(2, item?.Quantity);
+            Assert.AreEqual(15.99m, item?.Value);
+        }
+    }
+}
